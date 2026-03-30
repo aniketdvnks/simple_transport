afb95438bd1af1a1928b8b44747105d0527edf3b
aniketdvnks <aniket@dvnks.com>
2026-03-30 10:14:21 +0530

    Updated Sales Invoice customisation

---
+++ simple_transport/simple_transport/doctype/fuel_request/fuel_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, DVNKS Systems Pvt Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Fuel Request", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ simple_transport/simple_transport/doctype/fuel_request/test_fuel_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, DVNKS Systems Pvt Ltd and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFuelRequest(FrappeTestCase):
+	pass
